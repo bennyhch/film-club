576cd531d554c2b27536b570c89034072aeb9a1a
Ben Ho <bennyhch@gmail.com>
2022-03-10 17:53:28 +0000

    testing on a new branch

--- server/index.js
+++ server/index.js
@@ -9,24 +9,24 @@ const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const session = require('express-session');
 const SECRETKEY = process.env.SECRETKEY || 'this is not very secure';
-const SQLiteStore = require('connect-sqlite3')(session);
+// const SQLiteStore = require('connect-sqlite3')(session);
 
 app.use(cors());
-app.use(
-	session({
-		store: new SQLiteStore,
-		name: 'sessionid',
-		saveUninitialized: false,
-		resave: false,
-		secret: SECRETKEY,
-		cookie: {
-			maxAge: 1000 * 60 * 60 * 24, // 1day
-			sameSite: true,
-			httpOnly: false,
-			secure: false,
-		},
-	})
-);
+// app.use(
+// 	session({
+// 		store: new SQLiteStore,
+// 		name: 'sessionid',
+// 		saveUninitialized: false,
+// 		resave: false,
+// 		secret: SECRETKEY,
+// 		cookie: {
+// 			maxAge: 1000 * 60 * 60 * 24, // 1day
+// 			sameSite: true,
+// 			httpOnly: false,
+// 			secure: false,
+// 		},
+// 	})
+// );
 app.use(cookieParser());
 app.use(express.json());
 
